dc7cc20e0a2655c32c56ccb06b31213bee300f33
Alejandro Salazar Mejía <alejosalazarmejia.2000@gmail.com>
2023-06-22 17:21:06 -0500

    version 1

--- gym_examples/__init__.py
+++ gym_examples/__init__.py
@@ -11,3 +11,9 @@ register(
      entry_point="gym_examples.envs:GridWorldRandEnv",
      max_episode_steps=300,
 )
+
+register(
+     id="gym_examples/GridWorldRandom-v1",
+     entry_point="gym_examples.envs:GridWorldRandEnv_v1",
+     max_episode_steps=300,
+)
